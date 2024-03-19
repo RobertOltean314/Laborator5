962fb0b403445d1809c1e86a7a84293fa197c30d
Robert Oltean <olteanrobertadrian314@gmail.com>
2024-03-19 14:03:59 +0200

    Working

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,65 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        String file = "D:\\Paradigme de Programare\\Paradigme\\Laborator5\\src\\fisierTest";
+    static String filePath = "D:\\Paradigme de Programare\\Paradigme\\Laborator5\\src\\fisierTest";
 
+    public static void afiseaza(List<String> list) {
+        File file = new File(filePath);
         try {
-            // Citim din fisier
-            List<String> lines = Files.readAllLines(Paths.get(file));
-
-            // Afisam continutul fisierului
-            for (String line : lines) {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
                 System.out.println(line);
+                list.add(line);
             }
-            System.out.println();
-            // Sortam liniile si le afisam
-            Collections.sort(lines);
-            for (String line : lines) {
-                System.out.println(line);
-            }
-            System.out.println();
-            // Adaugam cuvintele in HashSet
-            Set<String> words = new HashSet<>();
-            for (String line : lines) {
-                for (String word : line.split("\\s+")) {
-                    words.add(word.toLowerCase());
-                }
-            }
-            // Verificam daca exista cuvantul "mere" in HashSet
-            if (words.contains("mere")) {
-                System.out.println("Exista");
-            } else {
-                System.out.println("Nu exista");
-            }
-        } catch (IOException e) {
-            System.err.format("IOException: %s%n", e);
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + filePath);
         }
+    }
+
+    public static void sorteaza(List<String> list) {
+        Collections.sort(list, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+    }
+
+    public static void gasesteCuvant(List<String> list, String cuvantCautat) {
+        // Adaugam cuvintele in HashSet
+        Set<String> words = new HashSet<>();
+        for (String line : list) {
+            for (String word : line.split("\\s+")) {
+                words.add(word.toLowerCase());
+            }
+        }
+        // Verificam daca exista cuvantul "mere" in HashSet
+        if (words.contains(cuvantCautat.toLowerCase())) {
+            System.out.println("Exista");
+        } else {
+            System.out.println("Nu exista");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        List<String> lines = new ArrayList<>();
+        String cautat = "mere";
+
+        afiseaza(lines);
+        System.out.println();
+        sorteaza(lines);
+        System.out.println();
+        afiseaza(lines);
+        gasesteCuvant(lines, cautat);
+
     }
 }
